acdb526c83043abd8e0c30a314708e410a77ad5d
Stuart Lees <stuartlees@gmail.com>
2017-12-14 12:36:45 -0500

    Fix missing instantiation of global common-electron logger

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 const logger = require("./logger");
 const config = require("./config/config");
 const commonConfig = require("common-display-module");
+const modulePath = commonConfig.getModulePath(config.moduleName);
 
-let debugging = process.argv.slice(1).join(" ").indexOf("debug") > -1,
-  debug = (debugging ? (msg)=>{console.log(msg);} : ()=>{});
+global.log = require("rise-common-electron").logger(null, modulePath, config.moduleName);
 
 commonConfig.receiveMessages(config.moduleName).then((receiver) => {
 
   receiver.on("message", (message) => {
     switch(message.topic) {
       case "log":
-        debug(JSON.stringify(message.data));
-
         logger.log(message.data)
-          .catch((e)=>{
-            debug(e);
+          .catch((err)=>{
+            log.debug(err.message);
           });
         break;
     }
