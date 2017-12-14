23c2f784a44ec6f78f17e1e12c34686acd86d462
Stuart Lees <stuartlees@gmail.com>
2017-12-14 13:59:40 -0500

    Reset "-events.log" and "-detail.log" files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const logger = require("./logger");
 const config = require("./config/config");
 const commonConfig = require("common-display-module");
 const modulePath = commonConfig.getModulePath(config.moduleName);
+const baseBytes = 10;
+const expo = 5;
+const maxFileSizeBytes = Math.pow(baseBytes, expo);
 
 global.log = require("rise-common-electron").logger(null, modulePath, config.moduleName);
+
+log.resetLogFiles(maxFileSizeBytes);
 
 commonConfig.receiveMessages(config.moduleName).then((receiver) => {
 
